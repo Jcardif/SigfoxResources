64c6c9dbc99c0254d88929eae3936c56ce320ee5
Josh Cardif <josh.cardif14@outlook.com>
2019-10-04 00:35:58 +0300

    Add First Config for sigfox

--- Ultrasonic_sensor/Ultrasonic_sensor.ino
+++ Ultrasonic_sensor/Ultrasonic_sensor.ino
@@ -1,34 +1,79 @@
-/*
-* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
-*
-* by Dejan Nedelkovski,
-* www.HowToMechatronics.com
-*
-*/
+#include <Arduino.h>
+#include <ArduinoLowPower.h>
+#include <SigFox.h>
+
+
 // defines pins numbers
 const int trigPin = 9;
 const int echoPin = 10;
+
 // defines variables
 long duration;
 int distance;
+
 void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
+
+    /*Get Device Info
+    Run This funtion to get the ID and PAC after which yoi can safely comment
+    */
+    getDeviceId();
+
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    Serial.begin(9600); // Starts the serial communication
 }
+
 void loop() {
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+
+    // Calculating the distance
+    distance= duration*0.034/2;
+
+    //Send to Distance Data to SigFox
+    
+
+    // Prints the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.println(distance);
+
+}
+
+void getDeviceId(){
+
+    Serial.begin(9600);
+    while(!Serial) {};
+    if (!SigFox.begin()) {
+            Serial.println("Shield error or not present!");
+    }
+
+    String version = SigFox.SigVersion();
+    String ID = SigFox.ID();
+    String PAC = SigFox.PAC();
+
+    // Display module informations
+    Serial.println("MKRFox1200 Sigfox first configuration");
+    Serial.println("SigFox FW version " + version);
+    Serial.println("ID  = " + ID);
+    Serial.println("PAC = " + PAC);
+
+    Serial.println("");
+
+    Serial.println("Register your board on https://backend.sigfox.com/activate with provided ID and PAC");
+
+    delay(100);
+
+    // Send the module to the deepest sleep
+    SigFox.end();
+
+    return;
 }
